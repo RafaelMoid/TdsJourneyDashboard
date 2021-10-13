c2fe3f766ed79ec6d0526d4959dfa12009a4dcee
Rafael Varela <rafaelvrcr@gmail.com>
2021-10-13 17:20:40 -0300

    Sidebar Icons added

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,12 @@
 import './Sidebar.css';
 import '../../assets/logo.png';
+import { FaHome, FaTools , FaChartLine, FaPowerOff} from 'react-icons/fa';
+import { CgProfile } from 'react-icons/cg';
+import { HiOutlineDocumentReport } from 'react-icons/hi';
+import {AiOutlineCloseSquare} from 'react-icons/ai';
+import {MdLocalLibrary} from 'react-icons/md';
+import {FiInfo} from 'react-icons/fi';
+
 
 const Sidebar = ({sidebarOpen, closeSidebar}) => {
     return (
@@ -9,12 +16,45 @@ const Sidebar = ({sidebarOpen, closeSidebar}) => {
                     <img src={"logo.png"} alt="logo" />
                     <h1>Strateegia Dashboard</h1>
                 </div>
-                <i 
+                < AiOutlineCloseSquare
                 className="fa fa-times" 
                 id="sidebarIcon" 
-                onClick={() => closeSidebar()}
-                ></i>
+                onClick={() => closeSidebar()} />
             </div>
+
+            <div className="sidebar__menu">
+                <div className="sidebar__link active_menu_link">
+                    < FaHome/>
+                    <a href="#"> Dashboard</a>
+                </div>
+                
+                <h2><FaTools />  Ferramentas</h2>
+                <div className="sidebar__link">
+                    <CgProfile/>
+                    <a href="#"> Gerenciamento de conta</a>
+                </div>
+                <div className="sidebar__link">
+                    < HiOutlineDocumentReport />
+                    <a href="#"> Meus relatórios</a>
+                </div>
+                <div className="sidebar__link">
+                    < MdLocalLibrary />
+                    <a href="#"> Biblioteca de relatórios</a>
+                </div>
+                <div className="sidebar__link">
+                    <FaChartLine/>
+                    <a href="#">  Comparações avançadas</a>
+                </div>
+                <div className="sidebar__link">
+                    <FiInfo/>
+                    <a href="#"> Guias</a>
+                </div>
+                <div className="sidebar__link">
+                    < FaPowerOff />
+                    <a href="#"> Sign Out</a>
+                </div>
+            </div>
+
         </div>
     )
 }
